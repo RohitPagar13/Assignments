afc0b2cec2ac984a5712b210d95c66ecdc29cd4c
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-20 14:31:28 +0530

    [Rohit] Add: player starts at position 0

--- Snake_and_Ladder_Day3/Program.cs
+++ Snake_and_Ladder_Day3/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace Snake_and_Ladder_Day3
 {
+    class Player
+    {
+        int start=0;
+        int position;
+
+        public Player()
+        {
+            position=0;
+        }
+
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder");   
+            Console.WriteLine("Welcome to Snake and Ladder");
+
+            Player p1 = new Player();
+            
         }
     }
 }
