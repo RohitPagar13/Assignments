0b3dbc938203de56f471a68ad933061f3f38b566
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-20 12:51:53 +0530

    [Rohit] Add: Welcome message

---
+++ Snake_and_Ladder_Day3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Snake_and_Ladder_Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder");   
+        }
+    }
+}
