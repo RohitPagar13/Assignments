ae5dd93b01731219c2495e6f36e41b67c3abd3bd
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-20 11:41:36 +0530

    [Rohit] Add : co-ord modified

--- Line_Comparison_Day2/Program.cs
+++ Line_Comparison_Day2/Program.cs
@@ -2,18 +2,16 @@
 {
     class Line
     {
-        private int x1, y1, x2, y2;
+        private double x1, y1, x2, y2;
 
         public Line()
         {
-            Console.WriteLine("Enter x1 : ");
-            this.x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter y1 : ");
-            this.y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter x2 : ");
-            this.x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter y2 : ");
-            this.y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter (x1,y1) : ");
+            this.x1 = Convert.ToDouble(Console.ReadLine());
+            this.y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter (x2,y2) : ");
+            this.x2 = Convert.ToDouble(Console.ReadLine());
+            this.y2 = Convert.ToDouble(Console.ReadLine());
         }
 
         public double CalLength()
