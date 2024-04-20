faf8bc79c2a74cca71a1b9468b491d4b14f615ae
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-20 15:15:53 +0530

    [Rohit] Add: given choice for no play, ladder or snake

--- Snake_and_Ladder_Day3/Program.cs
+++ Snake_and_Ladder_Day3/Program.cs
@@ -15,6 +15,18 @@
             Random r = new Random();
             return r.Next(1, 7);
         }
+
+        public void Ladder(int dice)
+        {
+            position += dice;
+            Console.WriteLine(position);
+        }
+
+        public void Snake(int dice)
+        {
+            position-=dice;
+            Console.WriteLine(position);
+        }
     }
     internal class Program
     {
@@ -23,7 +35,18 @@
             Console.WriteLine("Welcome to Snake and Ladder");
 
             Player p1 = new Player();
-            p1.getRandomDice();
+            int dice = p1.getRandomDice();
+
+            Console.WriteLine("Enter no.: 1:No play, 2:Ladder, 3: Snake");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: Console.WriteLine("You have missed the chance to play"); break;
+                case 2: p1.Ladder(dice); break;
+                case 3: p1.Snake(dice); break;
+                case 0: Console.WriteLine("Enter valid choice"); break;
+            }
         }
     }
 }
