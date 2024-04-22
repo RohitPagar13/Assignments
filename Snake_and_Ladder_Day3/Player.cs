0ca3c861a3bcc5bdfbc438e30754d9f786ee4e8f
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-22 13:15:16 +0530

    [Rohit] Add: game with two players and winner among them

---
+++ Snake_and_Ladder_Day3/Player.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder_Day3
+{
+    internal class Player
+    {
+        private int position;
+        private int count;
+        private string name;
+
+        public Player(string name)
+        {
+            position = 0;
+            this.name = name;
+        }
+
+        public int getPosition()
+        {
+            return position;
+        }
+
+        public int getCount()
+        {
+            return count;
+        }
+
+        public int getRandomDice()
+        {
+            Random r = new Random();
+            return r.Next(1, 7);
+        }
+
+        public int getRandomChoice()
+        {
+            Random r = new Random();
+            return r.Next(1, 4);
+        }
+
+        public void Ladder(int dice)
+        {
+            count++;
+            if (100 - position >= dice)
+            {
+                position += dice;
+                Console.WriteLine("Yay !! " + this.name + " have got Ladder " + dice + " and now you are at position:  " + this.getPosition() + "\n");
+
+            }
+            else
+            {
+                Console.WriteLine("Oh-Shit !! " + this.name + " have got ladder " + dice + "  but you need only " + (100 - position) + " points to win. \n");
+            }
+
+        }
+
+        public void Snake(int dice)
+        {
+            count++;
+            position -= dice;
+            if (position < 0)
+            {
+                position = 0;
+            }
+            Console.WriteLine("Oh-no !! " + this.name + "  have got Snake " + dice + " and now you are at position: " + this.getPosition() + "\n");
+        }
+
+        public void PlayGame()
+        {
+            while (this.getPosition() < 100)
+            {
+                int dice = this.getRandomDice();
+                int choice = this.getRandomChoice();
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Oops !! " + this.name + " have missed the chance to play and you are at position: " + this.getPosition() + "\n");
+                        break;
+
+                    case 2:
+
+                        this.Ladder(dice);
+
+                        if (this.getPosition() == 100)
+                        {
+                            Console.WriteLine("Congratulations  " + this.name + " !!! You are winner. You took " + this.getCount() + " chances to win." + "\n");
+                        }
+
+                        this.PlayGame();
+
+                        break;
+
+                    case 3:
+                        this.Snake(dice);
+                        break;
+
+                }
+                break;
+            }
+        }
+    }
+}
--- Snake_and_Ladder_Day3/Program.cs
+++ Snake_and_Ladder_Day3/Program.cs
@@ -1,96 +1,34 @@
 ﻿namespace Snake_and_Ladder_Day3
 {
-    class Player
-    {
-        private int position;
-        private int count;
-
-        public Player()
-        {
-            position=0;
-        }
-        
-        public int getPosition()
-        {
-            return position;
-        }
-
-        public int getCount()
-        {
-            return count;
-        }
-
-        public int getRandomDice()
-        {
-            Random r = new Random();
-            return r.Next(1, 7);
-        }
-
-        public int getRandomChoice()
-        {
-            Random r = new Random();
-            return r.Next(1, 4);
-        }
-
-        public void Ladder(int dice)
-        {
-            count++;
-            if (100 - position >= dice)
-            {
-                position += dice;
-                Console.WriteLine("Yay !! You have got Ladder " + dice + " and now you are at position:  " + this.getPosition()+"\n");
-            }
-            else
-            {
-                Console.WriteLine("Oh-Shit !! You have got ladder " + dice + "  but you need only " + (100 - position) + " points to win. \n");
-            }
-            
-        }
-
-        public void Snake(int dice)
-        {
-            count++;
-            position -=dice;
-            if (position < 0 )
-            {
-                position = 0;
-            }
-            Console.WriteLine("Oh-no !! You have got Snake " + dice + " and now you are at position: " + this.getPosition() + "\n");
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder !! \nPress Any Key to play the game \n");
+            Console.WriteLine("Enter player 1 name: ");
+            string p1Name = Console.ReadLine();
+            Player p1 = new Player(p1Name);
+            Console.WriteLine("Enter player 2 name: ");
+            string p2Name = Console.ReadLine();
+            Player p2 = new Player(p2Name);
 
-            Player p1 = new Player();
-            int choice;
-            
-            while(p1.getPosition()<100)
+            while((p1.getPosition() < 100) || (p2.getPosition() < 100))
             {
-                choice = p1.getRandomChoice();
-                    switch (choice)
-                    {
-                        case 1: 
-                            Console.WriteLine("Oops !! You have missed the chance to play and you are at position: "+ p1.getPosition() + "\n"); 
-                            break;
+                p1.PlayGame();
+                if (p1.getPosition() == 100)
+                {
+                    break;
+                }
+                
+                p2.PlayGame();
+                if (p2.getPosition() == 100)
+                {
+                    break;
+                }
 
-                        case 2:
-                            int diceLadder = p1.getRandomDice();
-                            p1.Ladder(diceLadder);
-                             
-                            if (p1.getPosition() == 100) { 
-                                Console.WriteLine("Congratulations !!! You are winner. You took "+p1.getCount()+" chances to win."+"\n");
-                                }
-                            break;
-
-                        case 3:
-                            int diceSnake = p1.getRandomDice();
-                            p1.Snake(diceSnake);
-                            break;
-                    }
             }
+            
+            
             Console.WriteLine("Press Any Key to exit. \n");
             Console.ReadLine();
         }
