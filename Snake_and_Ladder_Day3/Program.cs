a24b249503e90a45960ca79d36c03a79a4803f39
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-22 11:43:27 +0530

    [Rohit] Add: Player gets exact winning position 100

--- Snake_and_Ladder_Day3/Program.cs
+++ Snake_and_Ladder_Day3/Program.cs
@@ -28,13 +28,16 @@
 
         public void Ladder(int dice)
         {
-            
-            position += dice;
-            if (position > 100)
+            if (10 - position >= dice)
             {
-                position = 100;
+                position += dice;
+                Console.WriteLine("Yay !! You have got Ladder " + dice + " and now you are at position:  " + this.getPosition()+"\n");
             }
-
+            else
+            {
+                Console.WriteLine("Oh-Shit !! You have got ladder " + dice + "  but you need only " + (10 - position) + " points to win \n");
+            }
+            
         }
 
         public void Snake(int dice)
@@ -51,38 +54,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder !! \nPress Enter to play the game");
+            Console.WriteLine("Welcome to Snake and Ladder !! \nPress Enter to play the game \n");
 
             Player p1 = new Player();
             int choice;
-
-            while(p1.getPosition()<100)
+            
+            while(p1.getPosition()<10)
             {
                 choice = p1.getRandomChoice();
-                Console.ReadLine();
                     switch (choice)
                     {
                         case 1: 
-                            Console.WriteLine("Oops !! You have missed the chance to play and you are at position: "+ p1.getPosition()); 
+                            Console.WriteLine("Oops !! You have missed the chance to play and you are at position: "+ p1.getPosition() + "\n"); 
                             break;
 
                         case 2:
                             int diceLadder = p1.getRandomDice();
                             p1.Ladder(diceLadder);
-                            Console.WriteLine("Yay !! You have got Ladder "+ diceLadder + " and now you are at position:  " + p1.getPosition()); 
-                            if (p1.getPosition() >= 100) { 
-                                Console.WriteLine("Congratulations !!! You are winner.");
+                             
+                            if (p1.getPosition() == 10) { 
+                                Console.WriteLine("Congratulations !!! You are winner. \n");
                                 }
                             break;
 
                         case 3:
                             int diceSnake = p1.getRandomDice();
                             p1.Snake(diceSnake);
-                            Console.WriteLine("Oh-no !! You have got Snake "+ diceSnake + " and now you are at position: " + p1.getPosition());
+                            Console.WriteLine("Oh-no !! You have got Snake "+ diceSnake + " and now you are at position: " + p1.getPosition() + "\n");
                             break;
                     }
             }
-            Console.WriteLine("Press Enter to exit.");
+            Console.WriteLine("Press Enter to exit. \ns");
             Console.ReadLine();
         }
     }
