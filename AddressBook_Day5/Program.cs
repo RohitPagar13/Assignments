1b34d74802d3a3c9612d89729a620f61c44cfd6d
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-23 15:24:04 +0530

    [Rohit] Add: Welcome message

---
+++ AddressBook_Day5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBook_Day5
+{
+    internal class AddressBook
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
