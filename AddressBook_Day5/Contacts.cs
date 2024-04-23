cab689256c80478f736b8a92c5b0526fdab651c2
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-23 18:41:29 +0530

    [Rohit] Add: Edit Existing contact with first name and Display record using First name

--- AddressBook_Day5/Contacts.cs
+++ AddressBook_Day5/Contacts.cs
@@ -17,7 +17,44 @@ namespace AddressBook_Day5
         private string phone;
         private string email;
 
-        
+        public string getFName()
+        {
+            return firstName;
+        }
+
+        public string getLName()
+        {
+            return lastName;
+        }
+        public string getAddress()
+        {
+            return address;
+
+        }
+
+        public string getCity()
+        {
+            return city;
+        }
+
+        public string getState()
+        {
+            return state;
+        }
+        public string getZip()
+        {
+            return zip;
+        }
+        public string getPhone()
+        {
+            return phone;
+        }
+        public string getEmail ()
+        {
+            return email;
+
+        }
+
         public Contacts() {
             
             Console.WriteLine("Enter First Name: ");
@@ -44,6 +81,49 @@ namespace AddressBook_Day5
             Console.WriteLine("Enter email: ");
             this.email = Console.ReadLine();
 
+        }
+
+        
+
+        public void UpdateContact()
+        {
+
+            Console.WriteLine("Previous First Name: "+this.firstName+"\nEnter First Name: ");
+            this.firstName = Console.ReadLine();
+
+            Console.WriteLine("Previous Last Name: "+this.lastName+"\nEnter Last Name: ");
+            this.lastName = Console.ReadLine();
+            
+            Console.WriteLine("Previous Address: "+this.address+"\nEnter Address: ");
+            this.address = Console.ReadLine();
+            
+            Console.WriteLine("Previous City: "+this.city+"\nEnter City: ");
+            this.city = Console.ReadLine();
+
+            Console.WriteLine("Previous State: "+this.state+"\nEnter State: ");
+            this.state = Console.ReadLine(); ;
+
+            Console.WriteLine("Previous Zip: "+this.zip+"\nEnter Zip: ");
+            this.zip = Console.ReadLine();
+
+            Console.WriteLine("Previous Phone: "+this.phone+"\nEnter PhoneNumber: ");
+            this.phone = Console.ReadLine();
+
+            Console.WriteLine("Previous Email: "+this.email+"\nEnter email: ");
+            this.email = Console.ReadLine();
+
+        }
+
+        public void DisplayRecord()
+        {
+            Console.WriteLine("First Name: "+this.firstName);
+            Console.WriteLine("Last Name: "+ this.lastName);
+            Console.WriteLine("Address: " + this.address);
+            Console.WriteLine("City: "+ this.city);
+            Console.WriteLine("State: " + this.state);
+            Console.WriteLine("Zip: "+ this.zip);
+            Console.WriteLine("Phone: " + this.phone);
+            Console.WriteLine("Email: " + this.email);
         }
     }
 }
--- AddressBook_Day5/Program.cs
+++ AddressBook_Day5/Program.cs
@@ -8,17 +8,71 @@
 
             while (true)
             {
-                Console.WriteLine("What you want to do? \n1: Add New Contact \n0:Exit");
+                Console.WriteLine("What you want to do? \n1: Add New Contact \n2: Display Record \n3: Edit details \n0: Exit \n");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 if (choice == 0)
-                {
+                { 
                     Console.WriteLine("Exiting...");
                     break;
                 }
                 switch (choice)
                 {
-                    case 1: AddressBook.Add(new Contacts()); break;
-                    default: Console.WriteLine("Enter valid choice"); break;
+                    case 1: 
+                        AddressBook.Add(new Contacts());
+                        Console.WriteLine("\nRecore added to the AddressBook");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nEnter Name to Display details");
+                        string dName = Console.ReadLine();
+
+                        if (AddressBook.Count != 0)
+                        {
+                            foreach (Contacts contacts in AddressBook)
+                            {
+                                if (contacts.getFName().Equals(dName))
+                                {
+                                    contacts.DisplayRecord();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nYou are not in the AddressBook");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nYou are not in the AddressBook\n");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\nEnter Name to Edit details");
+                        string name = Console.ReadLine();
+
+                        if (AddressBook.Count != 0)
+                        {
+                            foreach (Contacts contacts in AddressBook)
+                            {
+                                if (contacts.getFName().Equals(name))
+                                {
+                                    contacts.UpdateContact();
+                                    Console.WriteLine("\nRecord updated in the AddressBook");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\nYou are not in the AddressBook");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nYou are not in the AddressBook");
+                        }
+                        break;
+
+                    default: 
+                        Console.WriteLine("\nEnter valid choice"); 
+                        break;
                 }
             }
         }
