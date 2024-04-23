69410839ac2d6d5ab7d8b9cd7257363f8e07e0a7
Rohit Pagar <rohitpagar13@gmail.com>
2024-04-23 15:47:35 +0530

    [Rohit] Add: Create new contact in address book

---
+++ AddressBook_Day5/Contacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_Day5
+{
+    internal class Contacts
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private long zip;
+        private long phone;
+        private string email;
+
+        public Contacts(string firstName, string lastName, string address, string city, string state, long zip, long phone, string email) { 
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+
+
+    }
+}
--- AddressBook_Day5/Program.cs
+++ AddressBook_Day5/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
+            Console.WriteLine("Welcome to Address Book. \n");
+
+            Contacts c = new Contacts("Rohit","Pagar","Nampur", "Nashik", "Maharashtra",423204,9130928097,"rohitpagar13@gmail.com");
         }
     }
 }
